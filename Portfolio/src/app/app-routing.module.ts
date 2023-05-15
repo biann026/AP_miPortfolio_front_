ced0182c8b67d4f7484e7f4486d9132883b25c75
Bianca <biann026@gmail.com>
2023-05-15 17:13:02 -0300

    hard y soft skills edit y delete con backend

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia-laboral/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia-laboral/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/new-educacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NewSkillComponent } from './components/hardsoftskills/new-skill.component';
+import { EditSkillComponent } from './components/hardsoftskills/edit-skill.component';
+
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'login', component:LoginComponent},
   {path:'nuevaexp', component:NewExperienciaComponent},
   {path:'editexp/:id', component:EditExperienciaComponent},
+  {path:'nuevaedu', component:NeweducacionComponent},
+  {path:'editedu/:id', component:EditeducacionComponent},
+  { path: 'newskill', component: NewSkillComponent},
+  { path: 'editskill/:id', component: EditSkillComponent}
 ];
 
 @NgModule({
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftskills.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,6 +19,11 @@ import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from 'src/service/interceptor-service';
 import { NewExperienciaComponent } from '../app/components/experiencia-laboral/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia-laboral/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/new-educacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { HysComponent } from './components/hardsoftskills/hys.component';
+import { NewSkillComponent } from './components/hardsoftskills/new-skill.component';
+import { EditSkillComponent } from './components/hardsoftskills/edit-skill.component';
 
 
 @NgModule({
@@ -30,13 +34,18 @@ import { EditExperienciaComponent } from './components/experiencia-laboral/edit-
     SobreMiComponent,
     ExperienciaLaboralComponent,
     EducacionComponent,
-    HardsoftskillsComponent,
     ProyectsComponent,
     FooterComponent,
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    HysComponent,
+    NewSkillComponent,
+    EditSkillComponent,
+
   ],
   imports: [
     BrowserModule,
